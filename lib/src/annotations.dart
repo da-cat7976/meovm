b0b6d245d0a3622b82d990bbaf2fc7021576494b
da-cat7976 <gierr7976@gmail.com>
2025-09-02 22:16:59 +0300

    feat: Added annotations

---
+++ lib/src/annotations.dart
@@ -0,0 +1,13 @@
+import 'package:meta/meta_meta.dart';
+
+@Target({TargetKind.field})
+final class VmMember {
+  const VmMember({this.debugName});
+
+  final String? debugName;
+}
+
+@Target({TargetKind.classType})
+final class VmMemberDefinition {
+  const VmMemberDefinition();
+}
