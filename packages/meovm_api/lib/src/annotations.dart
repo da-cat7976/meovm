d7d7fb711d6812c8d77836d51ebcd3c925e4f071
da-cat7976 <gierr7976@gmail.com>
2025-09-06 16:32:31 +0300

    feat(#4): Added MeovmDepend annotation

--- packages/meovm_api/lib/src/annotations.dart
+++ packages/meovm_api/lib/src/annotations.dart
@@ -4,3 +4,10 @@ import 'package:meta/meta_meta.dart';
 final class Meovm {
   const Meovm();
 }
+
+@Target({TargetKind.field})
+final class MeovmDepend {
+  const MeovmDepend(this.dependOn);
+
+  final Symbol dependOn;
+}
