f3bc9bd47a38966956b663997ff8e67165762bcd
da-cat7976 <gierr7976@gmail.com>
2025-09-06 17:00:10 +0300

    feat(#4): Disabled flag & comparison in/of MeovmDepend

--- packages/meovm_api/lib/src/annotations.dart
+++ packages/meovm_api/lib/src/annotations.dart
@@ -7,9 +7,27 @@ final class Meovm {
 
 @Target({TargetKind.field})
 final class MeovmDepend {
-  const MeovmDepend(this.dependOn, {bool this.external = false});
+  const MeovmDepend(
+    this.dependOn, {
+    this.external = false,
+    this.disabled = false,
+  });
 
   final Symbol dependOn;
 
   final bool external;
+
+  final bool disabled;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is MeovmDepend &&
+          runtimeType == other.runtimeType &&
+          dependOn == other.dependOn &&
+          external == other.external &&
+          disabled == other.disabled;
+
+  @override
+  int get hashCode => dependOn.hashCode ^ external.hashCode ^ disabled.hashCode;
 }
