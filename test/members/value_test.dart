375ba47325010d25df180eb21410bf6f5c3be6b0
da-cat7976 <gierr7976@gmail.com>
2025-09-15 16:00:18 +0300

    test(#8): Added tests for ListMember & SetMember

--- test/members/value_test.dart
+++ test/members/value_test.dart
@@ -11,6 +11,8 @@ class ValueMembersTests {
   void run() {
     group('Value members', () {
       _valueUpdate();
+      _listMemberMutations();
+      _setMemberMutations();
     });
   }
 
@@ -62,6 +64,119 @@ class ValueMembersTests {
       expect(find.text('3'), findsOneWidget);
     });
   }
+
+  void _listMemberMutations() {
+    testWidgets('ListMember add/remove updates UI and dependents', (tester) async {
+      final vm = _ListMemberVm();
+
+      await tester.pumpWidget(
+        Directionality(
+          textDirection: TextDirection.ltr,
+          child: ViewModelDispatcher(
+            factory: () => vm,
+            param: null,
+            child: Builder(
+              builder: (context) {
+                final vm = context.useVM<_ListMemberVm>();
+                final list = vm.list;
+                final length = vm.length;
+
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    list.build(
+                      builder: (_, __) => Text(
+                        list.data.join(', '),
+                        key: const ValueKey('list'),
+                      ),
+                    ),
+                    length.build(
+                      builder: (_, __) => Text(
+                        length.data.toString(),
+                        key: const ValueKey('list_length'),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ),
+      );
+
+      expect(find.byKey(const ValueKey('list')), findsOneWidget);
+      expect(find.text('1'), findsNWidgets(2));
+
+      vm.list.add(2);
+      await tester.pump();
+      expect(find.text('1, 2'), findsOneWidget);
+      expect(find.text('2'), findsOneWidget);
+
+      vm.list.remove(1);
+      await tester.pump();
+      expect(find.text('2'), findsOneWidget);
+      expect(find.text('1'), findsOneWidget);
+    });
+  }
+
+  void _setMemberMutations() {
+    testWidgets('SetMember add/remove updates UI and dependents', (tester) async {
+      final vm = _SetMemberVm();
+
+      await tester.pumpWidget(
+        Directionality(
+          textDirection: TextDirection.ltr,
+          child: ViewModelDispatcher(
+            factory: () => vm,
+            param: null,
+            child: Builder(
+              builder: (context) {
+                final vm = context.useVM<_SetMemberVm>();
+                final set = vm.set;
+                final length = vm.length;
+
+                String sortedSetString() {
+                  final items = set.data.toList()..sort();
+                  return items.join(', ');
+                }
+
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    set.build(
+                      builder: (_, __) => Text(
+                        sortedSetString(),
+                        key: const ValueKey('set'),
+                      ),
+                    ),
+                    length.build(
+                      builder: (_, __) => Text(
+                        length.data.toString(),
+                        key: const ValueKey('set_length'),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ),
+      );
+
+      expect(find.byKey(const ValueKey('set')), findsOneWidget);
+      expect(find.text('1'), findsNWidgets(2));
+
+      vm.set.add(2);
+      await tester.pump();
+      expect(find.text('1, 2'), findsOneWidget);
+      expect(find.text('2'), findsOneWidget);
+
+      vm.set.remove(1);
+      await tester.pump();
+      expect(find.text('2'), findsOneWidget);
+      expect(find.text('1'), findsOneWidget);
+    });
+  }
 }
 
 class _ValueMemberVm extends ViewModel {
@@ -75,5 +190,33 @@ class _ValueMemberVm extends ViewModel {
   @override
   void setDependencies(ViewModelDependencySetter depend) {
     depend(value, resolved);
+  }
+}
+
+class _ListMemberVm extends ViewModel {
+  late final list = ListMember<int>(initial: [1]);
+
+  late final length = ValueMember<int>(resolver: (_) => list.data.length);
+
+  @override
+  List<ViewModelMember> get members => [list, length];
+
+  @override
+  void setDependencies(ViewModelDependencySetter depend) {
+    depend(list, length);
+  }
+}
+
+class _SetMemberVm extends ViewModel {
+  late final set = SetMember<int>(initial: {1});
+
+  late final length = ValueMember<int>(resolver: (_) => set.data.length);
+
+  @override
+  List<ViewModelMember> get members => [set, length];
+
+  @override
+  void setDependencies(ViewModelDependencySetter depend) {
+    depend(set, length);
   }
 }
