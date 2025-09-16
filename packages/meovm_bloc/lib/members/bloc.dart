3531b9a86c05e1f4cf741377eadd0b0e41bf3f44
da-cat7976 <gierr7976@gmail.com>
2025-09-16 21:38:51 +0300

    ref(#2): Reworked error handling, added documentation for BlocMember

--- packages/meovm_bloc/lib/members/bloc.dart
+++ packages/meovm_bloc/lib/members/bloc.dart
@@ -1,12 +1,33 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meovm/meovm.dart';
 import 'package:meovm_bloc/core/feature.dart';
+import 'package:meovm_bloc/utils/error_handling.dart';
 import 'package:meta/meta.dart';
 
 typedef BlocExtractor = B Function<B extends StateStreamable<S>, S>();
 
 typedef BlocResolver<B, S> = B Function(BlocExtractor extractor);
 
+/// A member that provides data from the [Bloc] and [Cubit], as well as access
+/// to it.
+///
+/// Integration with blocs should be done through this class in the following way:
+/// ```dart
+/// class SomeVm extends ViewModel {
+///   late final bloc = BlocMember(
+///     resolver: (extractor) => extractor<SomeBloc, SomeState>(),
+///   );
+///
+///   void doSomeStuff() {
+///     SomeState = bloc.data;
+///     bloc.bloc.add(SomeEvent());
+///   }
+/// }
+/// ```
+///
+/// As [Bloc]s & [Cubit]s don't emit errors via its streams, to handle errors
+/// with this member, simply add [MeovmErrorHandler] mixin to corresponding
+/// [Bloc]/[Cubit].
 class BlocMember<B extends StateStreamable<S>, S> extends StreamMemberBase<S> {
   BlocMember({
     required this.resolver,
@@ -41,16 +62,31 @@ class BlocMember<B extends StateStreamable<S>, S> extends StreamMemberBase<S> {
 
   @override
   Stream<S> resolve() {
+    final previous = _bloc;
     final bloc = _bloc = resolver(owner.getBloc);
-    if (bloc != _bloc) {
+    if (bloc != previous) {
+      _reattachErrorHandler(previous, bloc);
+
       onEvent(bloc.state);
     }
 
     return bloc.stream;
   }
 
-  @override
-  late StreamMemberErrorListener? onError = _onErrorInternal;
+  // ? The default error handler of StreamMemberBase is not used since blocs
+  // ? don't emit errors via stream.
+  void _reattachErrorHandler(
+    StateStreamable<S>? prev,
+    StateStreamable<S> current,
+  ) {
+    if (prev is MeovmErrorHandler<S>) {
+      prev.removeErrorHandler(_onErrorInternal);
+    }
+
+    if (current is MeovmErrorHandler<S>) {
+      current.addErrorHandler(_onErrorInternal);
+    }
+  }
 
   void _onErrorInternal(Object error, StackTrace stacktrace) {
     _onError?.call(error, stacktrace);
@@ -58,8 +94,23 @@ class BlocMember<B extends StateStreamable<S>, S> extends StreamMemberBase<S> {
     _error = BlocMemberError(error: error, stackTrace: stacktrace);
     notifyChanged();
   }
+
+  @override
+  void dispose() {
+    final bloc = _bloc;
+    if (bloc is MeovmErrorHandler<S>) {
+      bloc.removeErrorHandler(_onErrorInternal);
+    }
+
+    _bloc = _error = null;
+    super.dispose();
+  }
 }
 
+/// A class-container for errors caught from blocs and cubits.
+///
+/// Note that this will never work, unless corresponding controller have
+/// [MeovmErrorHandler] mixin added to it.
 @immutable
 final class BlocMemberError {
   final Object error;
