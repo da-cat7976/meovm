e88afb713a2e4c223148249947e9abe6f10195cd
da-cat7976 <gierr7976@gmail.com>
2025-09-16 17:26:36 +0300

    ref(#2): Extracted freezing to mixin

---
+++ packages/meovm/lib/src/members/freeze.dart
@@ -0,0 +1,90 @@
+import 'dart:async';
+
+import 'package:meovm/src/core/view_model.dart';
+import 'package:meta/meta.dart';
+
+/// A mixin that provides a way to freeze the current state of the member.
+///
+/// When frozen, the member will not update its state, but store updates
+/// for future use. On unfreeze, the member will update its state with the last
+/// skipped data.
+mixin FreezableDataMixin<T> on ViewModelMember {
+  /// The current state of the member.
+  ///
+  /// If frozen is active, it returns the last value returned by [resolve]
+  /// before the call to [freeze].
+  ///
+  /// If [T] is not nullable, then accessing this getter before
+  /// initialization and the first update will result in a throw of
+  /// [TypeError].
+  T get data {
+    final data = _data;
+    assert(data is T, 'ViewModel should be initialized first');
+
+    return data as T;
+  }
+
+  @protected
+  T? get dataOrNull {
+    return _data;
+  }
+
+  /// Updates data or stores it for future if frozen.
+  @protected
+  T updateData(T data) {
+    final $data = _data;
+    final frozen = _frozen && $data is T;
+    if (frozen) {
+      _skippedData = data;
+      return $data;
+    }
+
+    return _data = data;
+  }
+
+  /// Is freezing enabled.
+  ///
+  /// If was set to `true`, all updates that occurred since setting are
+  /// saved within this member.
+  ///
+  /// Repeated setting with same value are safe and do not result in any
+  /// changes.
+  bool get frozen => _frozen;
+
+  set frozen(bool value) {
+    if (!_frozen) {
+      _data = _skippedData ?? _data;
+      _skippedData = null;
+    }
+
+    _frozen = value;
+  }
+
+  T? _data;
+
+  T? _skippedData;
+
+  bool _frozen = false;
+
+  /// Executes the passed function, freezing the state during its execution.
+  ///
+  /// Equivalent to:
+  /// ```dart
+  /// someMember.freeze();
+  /// // code from action body
+  /// someMember.unfreeze();
+  /// ```
+  Future<R> doFrozen<R>(FutureOr<R> Function() action) async {
+    frozen = true;
+    final result = await action();
+    frozen = false;
+
+    return result;
+  }
+
+  @override
+  void dispose() {
+    _data = _skippedData = null;
+    super.dispose();
+  }
+}
