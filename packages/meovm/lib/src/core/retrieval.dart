4f79452b794b2ae0fd07c213f8b116ce135411db
da-cat7976 <gierr7976@gmail.com>
2025-09-22 18:15:24 +0300

    feat(#18): Added scope implementation

--- packages/meovm/lib/src/core/retrieval.dart
+++ packages/meovm/lib/src/core/retrieval.dart
@@ -1,10 +1,12 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 
 import 'api.dart';
+import 'dispatcher.dart';
 
 class ViewModelProvider<VM extends ViewModelLifecycle> extends InheritedWidget {
   ViewModelProvider({required super.child, required this.viewModel})
-      : super(key: ValueKey(viewModel));
+    : super(key: ValueKey(viewModel));
 
   final VM viewModel;
 
@@ -15,7 +17,7 @@ class ViewModelProvider<VM extends ViewModelLifecycle> extends InheritedWidget {
 
 class ViewModelParamProvider<Param> extends InheritedWidget {
   ViewModelParamProvider({required super.child, required this.param})
-      : super(key: ValueKey(param));
+    : super(key: ValueKey(param));
 
   final Param param;
 
@@ -29,47 +31,145 @@ class ViewModelParamProvider<Param> extends InheritedWidget {
 // coverage:ignore-start
 extension ViewModelContext on BuildContext {
   // ignore: strict_raw_type
-  VM useVM<VM extends ViewModelLifecycle>({bool listen = true}) {
-    final inherited = listen
-        ? dependOnInheritedWidgetOfExactType<ViewModelProvider<VM>>()
-        : getInheritedWidgetOfExactType<ViewModelProvider<VM>>();
+  /// {@template vm_context.use_vm}
+  ///
+  /// Retrieves a ViewModel of type [VM] from the widget tree.
+  ///
+  /// If [scope] is `true`, also looks for scope and allows you to retrieve
+  /// a ViewModel by its super-type. Note:
+  /// 1. It is a relative expensive operation.
+  /// 2. It works only if `scope: true` is passed to [ViewModelDispatcherBase].
+  ///
+  /// If [listen] is `true`, subscribes current element to the replacements
+  /// of the ViewModel.
+  ///
+  /// {@endtemplate}
+  VM useVM<VM extends ViewModelLifecycle>({
+    bool listen = true,
+    bool scope = false,
+  }) {
+    final vm = useVmOrNull<VM>(listen: listen, scope: scope);
 
-    assert(
-    inherited is ViewModelProvider<VM>,
-    'No ViewModel with type $VM found in the widget tree',
-    );
-
-    return inherited!.viewModel;
+    assert(vm is VM, 'No ViewModel with type $VM found in the widget tree');
+    return vm!;
   }
 
   // ignore: strict_raw_type
-  VM? useVmOrNull<VM extends ViewModelLifecycle>({bool listen = true}) {
-    final inherited = listen
-        ? dependOnInheritedWidgetOfExactType<ViewModelProvider<VM>>()
-        : getInheritedWidgetOfExactType<ViewModelProvider<VM>>();
-
-    return inherited?.viewModel;
-  }
-
-  Param useParam<Param>({bool listen = true}) {
-    final inherited = listen
-        ? dependOnInheritedWidgetOfExactType<ViewModelParamProvider<Param>>()
-        : getInheritedWidgetOfExactType<ViewModelParamProvider<Param>>();
-
-    assert(
-    inherited is ViewModelParamProvider<Param>,
-    'No ViewModel param with type $Param found in the widget tree',
+  /// {@macro vm_context.use_vm}
+  VM? useVmOrNull<VM extends ViewModelLifecycle>({
+    bool listen = true,
+    bool scope = false,
+  }) {
+    final inherited = _listenAwareRetrieveInherited<ViewModelProvider<VM>>(
+      listen,
     );
 
-    return inherited!.param;
+    VM? vm = inherited?.viewModel;
+
+    if (scope && vm == null) {
+      final scope = _listenAwareRetrieveInherited<ViewModelScope>(listen);
+
+      vm = scope?.getVm<VM>();
+    }
+
+    return vm;
   }
 
-  Param? useParamOrNull<Param>({bool listen = true}) {
-    final inherited = listen
-        ? dependOnInheritedWidgetOfExactType<ViewModelParamProvider<Param>>()
-        : getInheritedWidgetOfExactType<ViewModelParamProvider<Param>>();
+  /// {@template vm_context.use_param}
+  ///
+  /// Retrieves a ViewModel parameter of type [Param] from the widget tree.
+  ///
+  /// If [scope] is `true`, also looks for scope and allows you to retrieve
+  /// a ViewModel parameter by its super-type. Note:
+  /// 1. It is a relative expensive operation.
+  /// 2. It works only if `scope: true` is passed to [ViewModelDispatcherBase].
+  ///
+  /// If [listen] is `true`, subscribes current element to the replacements
+  /// of the ViewModel parameter.
+  ///
+  /// {@endtemplate}
+  Param useParam<Param extends ViewModelParameter>({
+    bool listen = true,
+    bool scope = false,
+  }) {
+    final param = useParamOrNull<Param>(listen: listen, scope: scope);
 
-    return inherited?.param;
+    assert(
+      param is Param,
+      'No ViewModel param with type $Param found in the widget tree',
+    );
+
+    return param!;
+  }
+
+  /// {@macro vm_context.use_param}
+  Param? useParamOrNull<Param extends ViewModelParameter>({
+    bool listen = true,
+    bool scope = false,
+  }) {
+    final provider =
+        _listenAwareRetrieveInherited<ViewModelParamProvider<Param>>(listen);
+
+    Param? param = provider?.param;
+
+    if (scope && param == null) {
+      final scope = _listenAwareRetrieveInherited<ViewModelScope>(listen);
+      param = scope?.getParam();
+    }
+
+    return param;
+  }
+
+  T? _listenAwareRetrieveInherited<T extends InheritedWidget>(bool listen) {
+    return listen
+        ? dependOnInheritedWidgetOfExactType<T>()
+        : getInheritedWidgetOfExactType<T>();
   }
 }
 // coverage:ignore-end
+
+class ViewModelScope extends InheritedWidget {
+  const ViewModelScope._({
+    required super.child,
+    required Map<ViewModelLifecycle, ViewModelParameter?> vms,
+  }) : _vms = vms;
+
+  factory ViewModelScope({
+    required BuildContext context,
+    required ViewModelLifecycle vm,
+    required ViewModelParameter? param,
+    required Widget child,
+  }) {
+    final parent = context.dependOnInheritedWidgetOfExactType<ViewModelScope>();
+
+    return ViewModelScope._(
+      vms: {...?parent?._vms, vm: param},
+      child: child, // fmt
+    );
+  }
+
+  final Map<ViewModelLifecycle, ViewModelParameter?> _vms;
+
+  @override
+  bool updateShouldNotify(covariant ViewModelScope oldWidget) {
+    const eq = MapEquality();
+
+    return eq.equals(_vms, oldWidget._vms);
+  }
+
+  VM? getVm<VM extends ViewModelLifecycle>() {
+    for (final vm in _vms.keys) {
+      if (vm is VM) return vm;
+    }
+
+    return null;
+  }
+
+  Param? getParam<Param extends ViewModelParameter>() {
+    for (final param in _vms.values) {
+      if (param is Param) return param;
+    }
+
+    return null;
+  }
+}
